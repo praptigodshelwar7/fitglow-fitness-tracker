d2d6961f765ddf0391d08db113d8408594b80056
Prapti Godshelwar <praptigodshelwar7@gmail.com>
2026-01-16 01:26:48 +0530

    register-fixed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -43,6 +43,8 @@ function Register() {
         <form onSubmit={handleRegister} className="space-y-4">
 
         <input
+            name="name"
+            value={form.name}
             autocomplete="name"
             placeholder="Name"
             className="w-full px-4 py-2.5 bg-slate-800 text-white rounded-lg border border-slate-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -50,6 +52,8 @@ function Register() {
         />
 
         <input
+            name="email"
+            value={form.email}
             autocomplete="email"
             type="email"
             placeholder="Email"
@@ -58,6 +62,8 @@ function Register() {
         />
 
         <input
+            name="password"
+            value={form.password}
             autocomplete="new-password"
             type="password"
             placeholder="Password"
